3d7ea5869c8cc5015a2bf08e28b54f6ef78a9dbe
wahiba-baazaoui <wahibabaazawi3@gmail.com>
2021-04-01 22:17:19 +0100

    create project

---
+++ resources/views/cv/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+
+      <form action="{{url('cvs/'.$cv->id)}}"  method="post">
+        <input type="hidden" name="_method" value="PUT">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    
+ 
+
+      <div class="form-group">
+          <label for="title">title</label>
+          <input type="text"  name="title"  class="form-control" placeholder="entré titre" value="{{$cv->titre}}">
+      </div>
+
+      <div class="form-group">
+        <label for="presentation" >presentation</label> <br>
+        <textarea name="presentation" id="" cols="30" rows="10"  >{{$cv->presentation}}</textarea>
+    </div>
+
+
+    <div class="form-group">
+        <button class="btn btn-primary  ">Updtae</button>
+    </div>
+      </form>
+
+        </div>
+
+
+    </div>
+
+
+</div>
+    
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CvController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+
+Route::get('cvs',  [CvController::class, 'index']);
+Route::get('cvs/create',  [CvController::class, 'create']);
+Route::post('cvs',  [CvController::class, 'store']);
+Route::get('cvs/{id}/edit',  [CvController::class, 'edit']);
+Route::put('cvs/{id}',  [CvController::class, 'update']);
+Route::delete('cvs/{id}',  [CvController::class, 'destroy']);
+
+
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
